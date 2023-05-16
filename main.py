98ce5466b4016426f9791622f466fd78e1c41559
Andrew Duquette <andrewduquette@gmail.com>
2023-05-15 22:20:21 -0400

    Fixed CORS

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from spotify_watcher import SpotifyWatcher
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 client_id = os.getenv("SPOTIFY_CLIENT_ID")
@@ -10,6 +11,17 @@ refresh_token = os.getenv("SPOTIFY_REFRESH_TOKEN")
 
 sp = SpotifyWatcher(client_id=client_id, client_secret=client_secret, refresh_token=refresh_token)
 app = FastAPI()
+
+# CORS (allows requests from any origin)
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 def home():
