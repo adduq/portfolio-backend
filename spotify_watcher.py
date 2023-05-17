8184d882561c81533cd1cb020a841a4ef3dc5130
Andrew Duquette <andrewduquette@gmail.com>
2023-05-17 09:33:15 -0400

    Added track & artist url, removed is_playing field

--- spotify_watcher.py
+++ spotify_watcher.py
@@ -12,12 +12,11 @@ class SpotifyWatcher:
         self.token_expires_at = None
         self.last_fetched_at = None
         self.current_track: dict = None
-    
+
     def __get_access_token(self):
         if self.access_token is not None and self.token_expires_at is not None and time.time() < self.token_expires_at:
-            print("Using cached access token")
             return self.access_token
-        
+
         url = "https://accounts.spotify.com/api/token"
         payload = {
             "grant_type": "refresh_token",
@@ -27,16 +26,16 @@ class SpotifyWatcher:
         client_creds_b64 = base64.b64encode(client_creds.encode())
         headers = {"Authorization": f"Basic {client_creds_b64.decode()}"}
         response = requests.post(url, data=payload, headers=headers)
-        print(response.json())
-        self.token_expires_at = time.time() + response.json()["expires_in"]
-        self.access_token = response.json()["access_token"]
+        data = response.json()
+        self.token_expires_at = time.time() + data["expires_in"]
+        self.access_token = data["access_token"]
         return self.access_token
-    
+
     def get_currently_playing(self) -> dict:
         if self.last_fetched_at is not None and time.time() - self.last_fetched_at < 5:
             print("Using cached currently playing")
             return self.current_track
-        
+
         url = "https://api.spotify.com/v1/me/player/currently-playing"
         headers = {"Authorization": f"Bearer {self.__get_access_token()}"}
         response = requests.get(url, headers=headers)
@@ -44,16 +43,16 @@ class SpotifyWatcher:
         if response.status_code == 204:
             self.current_track = None
             return None
-        
+
         data = response.json()
         track = {
             "name": data["item"]["name"],
             "artist": data["item"]["artists"][0]["name"],
             "image_url": data["item"]["album"]["images"][0]["url"],
+            "track_url": data["item"]["external_urls"]["spotify"],
+            "artist_url": data["item"]["artists"][0]["external_urls"]["spotify"],
             "duration_ms": data["item"]["duration_ms"],
             "progress_ms": data["progress_ms"],
-            "is_playing": data["is_playing"],
         }
         self.current_track = track
         return track
-    
